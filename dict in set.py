47769b3d31826f5524a1c5d2a355596d5888a3e1
simrankaur2023 <140162757+simrankaur2023@users.noreply.github.com>
2024-02-22 10:36:19 +0530

    Add files via upload

---
+++ dict in set.py
@@ -0,0 +1,35 @@
+# mostlly used data set , collection of key value pairs and unordered collection.
+# create dictonry
+dict = {"name" :"simmi","age" :"21","branch" :"cse"}
+print(dict)
+
+# add new value pair
+
+dict["occupation"] ="software developer"
+print(dict)
+
+# accessing value
+name = dict["name"]
+print(name)
+
+# updating
+dict['age'] = 22
+print(dict)
+
+# removing
+del dict['branch']
+print(dict)
+
+# key, values, items
+
+keys = dict.keys()
+values = dict.values()
+items = dict.values()
+
+print(keys, values, items)
+
+
+
+
+
+
